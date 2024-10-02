ecac7c030aa55ba0b901445d086cff54593629b9
Chaitanya <130588638+chaitanya168@users.noreply.github.com>
2024-10-02 12:42:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,126 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+var swiper = new Swiper(".featured-slider", {
+  loop: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  autoplay:{
+      delay: 9500,
+      disableOnInteraction:false,
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  breakpoints:{
+      0:{
+          slidesPerView: 1,
+      },
+      450:{
+          slidesPerView: 2,
+      },
+      768:{
+          slidesPerView: 3,
+      },
+      1200:{
+          slidesPerView: 4,
+      },
+  },
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__tag", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btns", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".service__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+});
+
+ScrollReveal().reveal(".client__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+ScrollReveal().reveal(".client__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".client__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".client__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".client__details", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".client__rating", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+
+ScrollReveal().reveal(".download__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".download__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".download__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".download__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".download__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
